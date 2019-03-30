0f55a225c9673a953495adb0f87e3f7396ef1a9e
ElectricNinja315 <suzac314@gmail.com>
2019-03-30 15:33:05 -0700

    um

--- cogs/testbank.py
+++ cogs/testbank.py
@@ -1,4 +1,6 @@
 from discord.ext import commands
+from cogs.formulas import quad
+from random import randint
 
 import discord
 
@@ -7,6 +9,21 @@ class TestBank(commands.Cog):
 
     def __init__(self, bot):
         self.bot = bot
+    
+    @commands.command()
+    async def testbank(self, ctx, subject : str):
+        """Selects random questions from a larger test bank."""
+
+        if subject == "precalc":
+            a = randint(1, 9)
+            b = randint(-9, 9)
+            c = randint(-9, 9)
+
+            eq = f"Q: {a}x^2 + ({b})x + ({c})\n"
+            ans = "A: quad(a, b, c)"
+            ctx.send(eq + ans)
+
+
 
 
 def setup(bot):
